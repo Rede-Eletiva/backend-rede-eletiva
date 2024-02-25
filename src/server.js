f5dcefb911c0919f2bfb0d934214c4b44ef1cb6d
dev-edsonlopes <dev_edson@outlook.com>
2024-02-25 14:00:39 -0300

    configuração cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import fastify from "fastify";
 import { studentsRouter } from "./routes/studentsRouter.js";
 import { discplineRoutes } from "./routes/disciplineRouter.js";
+import cors from "@fastify/cors";
 
 const app = fastify();
 
 app.register(studentsRouter, { prefix: '/api/v1/students' });
 app.register(discplineRoutes, { prefix: '/api/v1/discipline' });
+
+app.register(cors, { origin: "*" });
 
 app.listen(
   { port: process.env.PORT || 3000, host: process.env.HOST || "0.0.0.0" },
