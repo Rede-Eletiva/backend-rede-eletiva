bc09d0a17aa238e294c07f268860b82d2b65ba54
dev-edsonlopes <dev_edson@outlook.com>
2024-03-03 20:33:50 -0300

    list all electives from silvio

--- src/models/disciplineModel.js
+++ src/models/disciplineModel.js
@@ -32,6 +32,34 @@ export class DiciplineModel {
         WHEN e.frame = 'sexta-feira' THEN 5
       END`;
 
+      if(module === '0') {
+        return await sql `SELECT 
+        e.code_elective,
+        e.name AS name_elective,
+        e.name_teacher,
+        e.number_vacancies AS total_vacancies,
+        COUNT(s.ra) AS filled_vacancies,
+        e.number_vacancies - COUNT(s.ra) AS available_vacancies,
+        e.frame
+      FROM 
+        electives e
+      LEFT JOIN 
+        choice_electives AS ce
+          ON ce.code_elective = e.code_elective
+      LEFT JOIN
+        students AS s
+          ON s.ra = ce.ra
+      GROUP BY 
+        e.code_elective, e.name, e.number_vacancies
+      ORDER BY 
+        CASE 
+          WHEN e.frame = 'segunda-feira' THEN 1
+          WHEN e.frame = 'terça-feira' THEN 2
+          WHEN e.frame = 'quarta-feira' THEN 3
+          WHEN e.frame = 'quinta-feira' THEN 4
+          WHEN e.frame = 'sexta-feira' THEN 5
+        END`;
+      }
     return disciplinesClasse;
   }
 
